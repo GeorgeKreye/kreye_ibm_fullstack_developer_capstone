4a55cde38c16b0e4c67d60d4aa1dc24b22e35913
GeorgeKreye <102697450+GeorgeKreye@users.noreply.github.com>
2025-07-03 17:01:04 -0400

    add review page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -134,7 +134,6 @@ def get_dealer_details(request,dealer_id):
     if dealer_id:
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
-        print(dealership)
         return JsonResponse({"status":200,"dealer":dealership})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})

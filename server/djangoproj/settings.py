2b5a3c48234dd2d6b1bca203747f35b0f1aaa63e
George Kreye <102697450+GeorgeKreye@users.noreply.github.com>
2026-03-08 17:13:39 -0600

    Add Render to allowed hosts for portfolio display

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = [
     'https://gjkreye-8000.theianext-0' +
     '-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
     'https://gjkreye-8000.theiadockernext-1-' +
-    'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://kreye-ibm-fullstack-developer-capstone.onrender.com/'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://gjkreye-8000.theianext-1-' +
